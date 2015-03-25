119c5ab97f8b9f23768a657ed99bfa4cc962dee7
Parthan Kasarapu <pkas7@student.monash.edu>
2015-03-25 20:33:40 +1100

    modified:   visualize/scripts/heat_map_3D.m

--- visualize/scripts/heat_map_3D.m
+++ visualize/scripts/heat_map_3D.m
@@ -11,4 +11,17 @@ density = M(:,4);
 
 h=scatter3(x,y,z,'cdata',density);
 
+view ([90 180]);
+
+%set(gca,'XTickLabel','','YTickLabel','','ZTickLabel','');
+%set(gca,'XTickLabel','');
+%set(gca,'YTickLabel','');
+%set(gca,'ZTickLabel','');
+
+set(gca,'xtick',[]);
+set(gca,'ytick',[]);
+set(gca,'ztick',[]);
+
+%saveas(gcf,'../ecc_1.eps','epsc');
+
 end
