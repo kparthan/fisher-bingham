097e685e1a54895f682e6e15af02f0aaf32aca68
Parthan Kasarapu <pkas7@student.monash.edu>
2015-05-10 13:13:40 +1000

    Updated

--- visualize/scripts/heat_map_2D.m
+++ visualize/scripts/heat_map_2D.m
@@ -1,16 +1,20 @@
 function [] = heat_map_2D(file_name)
 
-%[theta phi] = meshgrid(0:1:359.9,0:1:179.9);
-theta = 0:1:179.9;
-phi = 0:1:359.9;
-M = load(file_name);
-mesh(M);
-%meshc(phi,theta,M);
-%contour(phi,theta,M,1);
-xlabel('Longitude');
-ylabel('Co-latitude');
-zlabel('Z');
+  %[theta phi] = meshgrid(0:1:359.9,0:1:179.9);
+  theta = 0:1:179.9;
+  phi = 0:1:359.9;
 
-set(gca,'XLim',[0 360]);
-%set(gca,'YLim',[0 125]);
-%set(gca,'ZLim',[-5 5]);
+  M = load(file_name);
+
+  mesh(M);
+  %bar3(M);
+  %colormap(jet);
+
+  xlabel('Longitude');
+  ylabel('Co-latitude');
+  zlabel('Z');
+
+  set(gca,'XLim',[0 360]);
+  %set(gca,'YLim',[0 125]);
+
+end
--- visualize/scripts/visualize_mixture_contours_cdf.m
+++ visualize/scripts/visualize_mixture_contours_cdf.m
@@ -2,13 +2,14 @@ function [] = visualize_mixture_contours_cdf(K)
 
   addpath('export_fig');
 
-  bins_folder = '../sampled_data/bins_kent/';
-  outfile = 'kent_mix';
-  %bins_folder = '../sampled_data/bins_vmf/';
-  %outfile = 'vmf_mix';
+  %bins_folder = '../sampled_data/bins_kent/';
+  %outfile = 'kent_mix';
+  bins_folder = '../sampled_data/bins_vmf/';
+  outfile = 'vmf_mix';
  
   % figure properties
   fig = figure();
+  caxis([0 1]); % colormap range
   %axis equal
   hold on;
   set(gcf, 'Color', 'w');
@@ -22,21 +23,26 @@ function [] = visualize_mixture_contours_cdf(K)
   % plot the contours 
   phi = 0:1:359.9;  % meshgrid columns
   theta = 0:1:179.9;  % meshgrid rows 
-  min_val = 0;
-  max_val = 0;
-%  for k = 1:K
-%    %k
-%    data_file = strcat(bins_folder,'comp',num2str(k),'_prob_bins2D.dat');
-%
-%    prob_bins = load(data_file);
-%    [max_val_bins max_index] = max(prob_bins(:));
-%    prob_bins = prob_bins / sum(sum(prob_bins));
-%    [sorted_prob_bins, indexes] = sort(prob_bins(:),1,'descend');
-%    sorted_prob_bins_cumsum = cumsum(sorted_prob_bins);
-%    cdf_bins_list = ones(length(sorted_prob_bins),1);
-%    cdf_bins_list(indexes) = sorted_prob_bins_cumsum;
-%    cdf_bins = reshape(cdf_bins_list,size(prob_bins));
-%
+%  min_val = 0;
+%  max_val = 0;
+  for k = 1:K
+    %k
+    data_file = strcat(bins_folder,'comp',num2str(k),'_prob_bins2D.dat');
+
+    prob_bins = load(data_file);
+    [max_val_bins max_index] = max(prob_bins(:));
+    prob_bins = prob_bins / sum(sum(prob_bins));
+    [sorted_prob_bins, indexes] = sort(prob_bins(:),1,'descend');
+    sorted_prob_bins_cumsum = cumsum(sorted_prob_bins);
+    cdf_bins_list = ones(length(sorted_prob_bins),1);
+    cdf_bins_list(indexes) = sorted_prob_bins_cumsum;
+    cdf_bins = reshape(cdf_bins_list,size(prob_bins));
+
+    min_val = min(cdf_bins(:));
+    max_val = max(cdf_bins(:));
+    range = max_val - min_val;
+    cdf_bins = (cdf_bins - min_val) / range; % in [0,1]
+
 %    if k == 1
 %      min_val = min(cdf_bins(:));
 %      max_val = max(cdf_bins(:));
@@ -50,25 +56,26 @@ function [] = visualize_mixture_contours_cdf(K)
 %        max_val = current_max;
 %      end
 %    end
-%
-%    %contour_levels = [0.5 0.5];
-%    contour_levels = [0.5 0.5];
-%    [C,h] = contour(cdf_bins,contour_levels,'LineWidth',1,'LineColor','black');
-%    %[C,h] = contour(norm_prob_bins,1,'LineWidth',2,'LineColor','black');
-%    %clabel(C,h);
-%
-%    [row col] = ind2sub(size(prob_bins),max_index);
-%    cx = phi(col);
-%    cy = theta(row);
-%    ht = text(cx,cy,num2str(k),'Color','red');
-%
-%%    hcl = clabel(C,'Color','red');
-%%    for i=2:2:length(hcl)
-%%      old_label = get(hcl(i),'String');
-%%      new_label = num2str(k);
-%%      set(hcl(i),'String',new_label);
-%%    end
-%  end  
+
+    level = 0.9;
+    norm_level = (level - min_val) / range;
+    contour_levels = [norm_level norm_level];
+    [C,h] = contour(cdf_bins,contour_levels,'LineWidth',2,'LineColor','black');
+    %[C,h] = contour(cdf_bins,1,'LineWidth',2,'LineColor','black');
+    %clabel(C,h);
+
+    [row col] = ind2sub(size(prob_bins),max_index);
+    cx = phi(col);
+    cy = theta(row);
+    ht = text(cx,cy,num2str(k),'Color','red');
+
+%    hcl = clabel(C,'Color','red');
+%    for i=2:2:length(hcl)
+%      old_label = get(hcl(i),'String');
+%      new_label = num2str(k);
+%      set(hcl(i),'String',new_label);
+%    end
+  end  
 
   %uistack(hs,'bottom');
   %uistack(ht,'top');
@@ -77,17 +84,13 @@ function [] = visualize_mixture_contours_cdf(K)
   data_file = strcat(bins_folder,'mixture_density.dat');
   M = load(data_file);
 
-  cmap = colormap()
-  size(cmap)
-  imwrite(cmap)
-
   density = M(:,4);
   min1 = min(density);
   max1 = max(density);
-  range1 = max1 - min1
+  range1 = max1 - min1;
   %range = max_val - min_val;
-  range = 1;
-  norm_density = min_val + (((density - min1) / range1) * range);
+  %norm_density = min_val + (((density - min1) / range1) * range);
+  norm_density = (density - min1) / range1; % in [0,1]
   n = size(M,1);
   angles = zeros(n,2);
   for i=1:n
@@ -98,17 +101,16 @@ function [] = visualize_mixture_contours_cdf(K)
     angles(i,1) = phi;
     angles(i,2) = theta;
   end
-%  min_val
-%  max_val
 %  min(norm_density)
 %  max(norm_density)
   hs = scatter3(angles(:,1),angles(:,2),norm_density,1,'cdata',norm_density);
 
+  %colorbar
   output_fig = strcat('../figs/',outfile,'.fig');
   output_eps = strcat('../figs/',outfile,'.eps');
   output_pdf = strcat('../figs/',outfile,'.pdf');
 
-  %saveas(gcf,output_fig);
+  saveas(gcf,output_fig);
   %print2eps(output_eps);
   %eps2pdf(output_eps,output_pdf,1);
   %export_fig(output_pdf,'-pdf');
